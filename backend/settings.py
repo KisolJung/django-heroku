6ce632659d6eda28ed544637c017c20882dcbcc4
KisolJung <wsall777@gmail.com>
2021-11-21 00:07:53 +0900

    setting key modified

--- backend/settings.py
+++ backend/settings.py
@@ -142,8 +142,8 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
+AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_S3_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
